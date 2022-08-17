1dbe9292ba6faf2d173092be467ef5d1b6f59380
MaverickSamurai <mirsamirba@code.edu.az>
2022-08-17 19:53:26 +0400

    JS-Intro-Practice

---
+++ JS-Intro-Practice/assets/js/script.js
@@ -0,0 +1,258 @@
+// alert("Hello");
+
+// console.log("Salamlar");
+
+// var,let,const
+
+// var name = "Mirsamir";
+// var name = "Sadiq";
+
+// let name = "Mirsamir";
+// let name1 = "Sadiq";
+
+// let name;
+// name="Elman";
+
+// var surname = "Azimzada";
+
+// const age = 22;
+
+// console.log(age);
+
+// console.log(name + " " + name1);
+
+// console.log(name + " " + surname);
+// console.log(`${name} ${surname}`);
+
+// let age = 22;
+// console.log(age);
+
+// let surname = ("Nahmatov");
+// console.log(surname);
+
+// let isMarried = true;
+// console.log(isMarried);
+
+// let adress = null;
+// console.log(adress);
+
+// let count;
+// console.log(count);
+
+// let person = {
+//     name: "Mirsamir",
+//     surname: "Azimzada",
+//     age: 22
+// };
+
+// console.log(person.name + " " + person.surname);
+
+// let students = ["Mirsamir", "Ramil", 44];
+
+// for (let i = 0; i < students.length; i++) {
+
+//     console.log(students[i]);
+// }
+
+// let person1 = {
+//     name: "Orxan",
+//     surname: "Akberov",
+//     age: 26,
+//     phoneNumbers: [2514852,1548512]
+// };
+
+// let person2 = {
+//     name: "Ramil",
+//     surname: "Binnatov",
+//     age: 18,
+//     phoneNumbers: [2514852,1548512]
+// };
+
+// let students = [person1,person2];
+
+// for (let i = 0; i < students.length; i++) {
+
+//     console.log(students[i]);
+// }
+
+// for (let i = 0; i < students.length; i++) {
+//    for (let k = 0; k < students[i].phoneNumbers.length; k++) {
+
+//     console.log(students[i].phoneNumbers[k]);
+
+//    }
+
+// }
+
+// for (let i = 0; i < person1.phoneNumbers.length; i++) {
+
+//     console.log(person1.phoneNumbers[i]);
+// }
+
+// let numbers = [1,2,3,4,5,6,7,8,9];
+
+// numbers.forEach(element => {
+//     console.log(element);
+// });
+
+
+// let person1 = {
+//     name: "Orxan",
+//     surname: "Akberov",
+//     age: 26,
+//     phoneNumbers: [2514852,1548512]
+// };
+
+// let person2 = {
+//     name: "Ramil",
+//     surname: "Binnatov",
+//     age: 18,
+//     phoneNumbers: [2514852,1548512]
+// };
+
+// let students = [{
+//     name: "Ramil",
+//     surname: "Binnatov",
+//     age: 18,
+//     phoneNumbers: [2514852, 1548512]
+// },
+// {
+//     name: "Orxan",
+//     surname: "Akberov",
+//     age: 26,
+//     phoneNumbers: [2514852, 1548512]
+// },
+
+// {
+//     name: "Emil",
+//     surname: "Abdullayev",
+//     age: 21,
+//     phoneNumbers: [2514852, 1548512]
+// }
+// ];
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(i);
+
+// }
+// console.log(i);
+
+// for (var i = 0; i < students.length; i++) {
+
+
+// }
+// console.log(i);
+
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i].name == "Orxan") {
+//         console.log(students[i]);
+//         break;
+//     }
+
+//     else(
+//         console.log("Not found")
+//     )
+
+// }
+
+// for (var i = 0; i < students.length; i++) {
+//     if(students[i].age<=22){
+//         console.log(students[i])
+//         break;
+//     }
+
+
+
+//     }
+
+// for (var i = 0; i < students.length; i++) {
+//     if (students[i].name == "Orxan") {
+//         console.log(students[i].age + " " + students[i].surname)
+//         break;
+//     }
+
+// }
+
+// let num1 = 66;
+// let num2 = 33;
+// let result = num1%num2;
+// console.log(result);
+
+// for (let i = 0; i <= 10 ; i++) {
+//     if(i == 0){
+//         console.log("Ne tek ne de cutdur")
+//     }
+//     else{
+//         if(i%2 == 1){
+//             console.log("Tek :" +i)
+//         }
+//         else(
+//             console.log("Cut :" +i)
+//         )
+//     }   
+    
+// }
+
+
+// let isActive = true;
+
+// if(isActive){
+//     console.log("Aktiv deyil");
+// }else{
+//     console.log("Aktivdir");
+// }
+    
+// let result = isActive? "Aktiv" : "Aktiv deyil";
+// console.log(result);
+
+// let surname = "Nahmatov";
+// let surname = null;
+
+// let result = surname?? "Familyasi yaddan cixib";
+
+// console.log(result);
+
+// let a = 5;
+// let b = a;
+
+// console.log(a);
+// console.log(b);
+
+// let obj1 = {
+//     name: "Orxan",
+//     surname: "Akberov",
+//     age: 26,
+//     phoneNumbers: [2514852, 1548512]
+// }
+// let obj2 = obj1;
+//     obj2.age = 77;
+
+//     console.log(obj1);
+
+// let studentName;
+
+// if(studentName == undefined){
+//     console.log("Teyin olunmayib");
+// }else{
+//    console.log("Teyin olunub");
+// }
+
+// let isDeleted = true;
+
+// console.log(typeof isDeleted);
+
+// let isDeleted = 55.6;
+
+// console.log(typeof isDeleted);
+
+// let isDeleted = null;
+
+// console.log(typeof isDeleted);
+
+// let isDeleted = undefined;
+
+// console.log(typeof isDeleted);
+
+// let isDeleted = "true";
+
+// console.log(typeof isDeleted);
